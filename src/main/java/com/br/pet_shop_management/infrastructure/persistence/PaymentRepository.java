a69959742c92277817a864f3d0ce142a8afba8c1
Andressa Soares <andressa012215@gmail.com>
2026-01-18 13:49:43 -0300

    feat: create payment repository

---
+++ src/main/java/com/br/pet_shop_management/infrastructure/persistence/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.br.pet_shop_management.infrastructure.persistence;
+
+import com.br.pet_shop_management.domain.entity.PaymentEntity;
+import com.br.pet_shop_management.domain.enums.PaymentStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
+    boolean existsByAppointmentIdAndStatus(Long appointmentId, PaymentStatus status);
+}
