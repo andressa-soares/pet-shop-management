6b5c5b37af66f54668c027f088595d3b668719a3
Andressa Soares <andressa012215@gmail.com>
2026-01-18 13:44:01 -0300

    feat: create payment enums

---
+++ src/main/java/com/br/pet_shop_management/domain/enums/PaymentMethod.java
@@ -0,0 +1,7 @@
+package com.br.pet_shop_management.domain.enums;
+
+public enum PaymentMethod {
+    PIX,
+    CASH,
+    CARD
+}
---
+++ src/main/java/com/br/pet_shop_management/domain/enums/PaymentStatus.java
@@ -0,0 +1,7 @@
+package com.br.pet_shop_management.domain.enums;
+
+public enum PaymentStatus {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
